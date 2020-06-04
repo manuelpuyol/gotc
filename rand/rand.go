bdeb94cdc8161ffc446338a947025a61b5f99e44
Manuel Puyol <manuelpuyol@gmail.com>
2020-06-04 15:36:07 -0500

    add random bytes generator

---
+++ rand/rand.go
@@ -0,0 +1,19 @@
+package rand
+
+import "math/rand"
+
+const ASCIIStart = 65
+const ASCIIEnd = 90
+const Length = 10
+
+func RandomBytes() []byte {
+	bytes := make([]byte, Length)
+	for i := 0; i < Length; i++ {
+		bytes[i] = byte(randomInt(ASCIIStart, ASCIIEnd))
+	}
+	return bytes
+}
+
+func randomInt(min, max int) int {
+	return min + rand.Intn(max-min)
+}
